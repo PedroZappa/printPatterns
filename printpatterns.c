1554d49d5e03ef6d8f5e264e7d0fb5ddf73d8605
PedroZappa <pedrogzappa@gmail.com>
2023-04-20 21:50:18 +0100

    Efficiency improvements, switch cases and puts()

--- printpatterns.c
+++ printpatterns.c
@@ -6,61 +6,77 @@ bool valid_pattern(int pattern) {
 }
 
 int main() {
-  // i = rows, j = spaces, k = stars;
-  int i, j, rows, k, pattern, space;
+  int row, space, star, pattern, rows;
 
   // Choose Pattern
-  printf("[ Pattern Printer ]\nSelect a Pattern:\n1: Half-Pyramid\n2: Full-Pyramid\n3: Inverted Half-Pyramid:\n4: Inverted Full-Pyramid:\n");
+  puts("[ Pattern Printer ]");
+  puts("Select a Pattern:");
+  puts("1: Half-Pyramid");
+  puts("2: Full-Pyramid");
+  puts("3: Inverted Half-Pyramid:");
+  puts("4: Inverted Full-Pyramid:");
   scanf("%d", &pattern);
+
   // Validate Input
   if (!valid_pattern(pattern)) {
     printf("Error! Invalid Pattern Code.\n");
     return 0;
   }
+
   // Take Rows Input
   printf("\nEnter the number of Rows: ");
   scanf("%d", &rows); 
 
-  if (pattern == 1) {
-    // Print Half-Pyramid Pattern
-    for (i = 1; i <= rows; ++i) { // Number o'Rows
-      for (j = 1; j <= i; ++j) { // Number o'Stars
-        printf("* ");
+  switch (pattern) {
+    case 1:
+      // Print Half-Pyramid Pattern
+      for (row = 1; row <= rows; ++row) { // Number o'Rows
+        for (star = 1; star <= row; ++star) { // Number o'Stars
+          printf("* ");
+        }
+        printf("\n");
       }
-      printf("\n");
-    }
-  } else if (pattern == 2) {
-    // Print Full-Pyramid Pattern
-    for (i = 1; i <= rows; i++, k = 0) { // Number o'Rows
-      for (j = 0; j < rows-i; j++) { // Number o'Spaces
-        printf(" ");
+      break;
+
+    case 2:
+      // Print Full-Pyramid Pattern
+      for (row = 1; row <= rows; row++) { // Number o'Rows
+        for (space = 1; space <= rows - row; space++) { // Number o'Spaces
+          printf("  ");
+        }
+        for (star = 1; star <= 2 * row - 1; star++) { // Number o'Stars
+          printf("* ");
+        }
+        printf("\n");
       }
-      for (k = 1; k <= 2*i-1; k++) { // Number o'Stars
-        printf("*");
+      break;
+
+    case 3:
+      // Print Inverted Half-Pyramid Pattern
+      for (row = rows; row >= 1; --row) {
+        for (star = 1; star <= row; ++star) {
+           printf("* ");
+        }
+        printf("\n");
       }
-      printf("\n");
-    }
-  } else if (pattern == 3) {
-    // Print Inverted Half-Pyramid Pattern
-    for (i = rows; i >= 1; --i) {
-      for (j = 1; j <= i; ++j) {
-         printf("* ");
+      break;
+
+    case 4:
+      // Print Inverted Full-Pyramid Pattern
+      for (row = rows; row >= 1; --row) {
+        for (space = 0; space < rows - row; ++space)
+           printf("  ");
+        for (star = row; star <= 2 * row - 1; ++star)
+           printf("* ");
+        for (star = 0; star < row - 1; ++star)
+           printf("* ");
+        printf("\n");
       }
-      printf("\n");
-   }
-  } else if (pattern == 4) {
-    // Print Inverted Full-Pyramid Pattern
-    for (i = rows; i >= 1; --i) {
-      for (space = 0; space < rows - i; ++space)
-         printf("  ");
-      for (j = i; j <= 2 * i - 1; ++j)
-         printf("* ");
-      for (j = 0; j < i - 1; ++j)
-         printf("* ");
-      printf("\n");
-    } 
-  } else {
-    printf("Error! Invalid Pattern Code.\n");
+      break;
+
+    default:
+      puts("Error! Invalid Pattern Code.");
+      break;
   }
 
   return 0;
